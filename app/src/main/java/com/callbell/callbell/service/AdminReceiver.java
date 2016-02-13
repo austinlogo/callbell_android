677449ba17bd43550655a84f2300551bd9e09bf6
Austin Lopez-Gomez <austinlg@vt.edu>
2016-02-13 14:03:55 -0800

    Added files needed for DeviceOwner

---
+++ app/src/main/java/com/callbell/callbell/service/AdminReceiver.java
@@ -0,0 +1,10 @@
+package com.callbell.callbell.service;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by austin on 2/12/16.
+ */
+public class AdminReceiver extends DeviceAdminReceiver {
+
+}
